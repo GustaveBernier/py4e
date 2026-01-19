0e8049f1896c07835bc203470c0a19485e0614c1
Gustave Bernier <gustave.bernier@gmail.com>
2026-01-19 23:49:08 +0100

    Undo HTML and JS minify (no diff)

--- Course_4/assets/opengeo/where.js
+++ Course_4/assets/opengeo/where.js
@@ -1,1 +1,3 @@
-myData = [[46.5879801,0.3596795229723561, 'École Européenne Supérieure de lImage (EESI)']];
+myData = [
+    [46.5879801,0.3596795229723561, 'École Européenne Supérieure de lImage (EESI)']
+];
